6d3abb2a94846a3b9a1f06c36ab724e53009bb77
Abstrekt <saammmmmmy@gmail.com>
2020-04-10 22:28:50 +0530

    Updates

---
+++ precache-manifest.e66d363863719b59f4e8129809677b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb46486bf62e2f209d8e587187e6d8d",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "554a5ec60136d333d490",
+    "url": "/Monsters-rolodex/static/css/main.ebcafd42.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554a5ec60136d333d490",
+    "url": "/Monsters-rolodex/static/js/main.0185f80c.chunk.js"
+  },
+  {
+    "revision": "a6902c3c61d39972738f",
+    "url": "/Monsters-rolodex/static/js/runtime-main.6218a32a.js"
+  }
+]);
